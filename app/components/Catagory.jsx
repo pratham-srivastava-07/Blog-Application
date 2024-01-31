cea33cf25a551071567cb7f2ac2753308a2148c5
Krish <prathamrajsri2003@gmail.com>
2024-01-31 22:17:44 +0530

    catagory list add

--- app/components/Catagory.jsx
+++ app/components/Catagory.jsx
@@ -8,15 +8,25 @@ const Catagory = () => {
     <div className='container '>
       <h1 className="title mt-30 text-white font-bold lg:ml-28">Popular Catagories</h1>
       <div className="flex flex-wrap w-30 lg:ml-32 justify-between mt-5 gap-20">
-        <Link href="/blog?cat=style" className="category flex text-white items-center gap-10 capitalize w-1/7 h-20 justify-center rounded-lg bg-blue-500 bg-opacity-30">
+        <Link href="/blog?cat=style" className="category flex text-center text-white items-center gap-5 capitalize w-1/7 h-20 justify-center rounded-lg bg-blue-500 bg-opacity-30">
           <Image 
            src="/style.png" 
            alt="style"
            width={32}
            height={32}
-           className='rounded-full ml-3'
+           className='rounded-full ml-3 mr-3 '
            />
            Style
+        </Link>
+        <Link href="/blog?cat=fashion" className='catagory flex items-center gap-10 capitalize w-1/7 h-20 text-white justify-center rounded-lg bg-yellow-500 bg-opacity-30'>
+        <Image 
+           src="/fashion.png" 
+           alt="fashion"
+           width={32}
+           height={32}
+           className='rounded-full ml-3 mr-3 '
+           />
+           Fashion
         </Link>
 
       </div>
