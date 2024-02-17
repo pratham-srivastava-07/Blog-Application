1d0bf69cfe387b878be2d1410c4400ad48090006
Krish <prathamrajsri2003@gmail.com>
2024-02-18 00:01:34 +0530

    init

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -6,19 +6,37 @@ export default function Menu() {
   return (
     <div className='container text-white flex-1 mt-9'>
       <h2 className="text-red-400 text-base font-normal">{"What's hot?"}</h2>
-      <h1 className="title text-3xl">Most Popular</h1>
+      <h1 className="title text-3xl mb-5">Most Popular</h1>
+    
       <div className="items text-xl">
-        <Link href="/" className='item flex items-center gap-5'>
-          <div className="imgContainer flex-1 relative">
-              <Image src="/coding.png" alt='' className='rounded-full' width={500} height={500}/>
+        <Link href={`/blog?popular=coding`} className='item flex items-center gap-5 mt-5'>
+          <div className="imgContainer  relative">
+              <Image src="/coding.png" alt='' className='rounded-full' width={200} height={200}/>
           </div>
-          <div className="textContainer flex-1">
-            <span className="text-white text-xl">Coding</span>
-            <h3 className="text-white">
+          <div className="textContainer  ">
+          <span className="text-white bg-red-600 w-auto rounded-xl p-1 text-sm ">Coding</span>
+            <h3 className="text-white text-md ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima?
             </h3>
-            <div className="name text-white">John Doe</div>
+           <div className="details flex text-xs space-x-3 mt-2">
+           <div className="name text-white">John Doe</div>
             <div className="date">{new Date().toLocaleDateString('en-US')}</div>
+           </div>
+          </div>
+        </Link>
+        <Link href={`/blog?popular=travel`} className='item flex items-center gap-5 mt-5'>
+          <div className="imgContainer  relative">
+              <Image src="/travel.png" alt='' className='rounded-full' width={200} height={50}/>
+          </div>
+          <div className="textContainer  ">
+          <span className="text-white bg-red-600 w-auto rounded-xl p-1 text-sm ">Travel</span>
+            <h3 className="text-white text-md ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima?
+            </h3>
+           <div className="details flex text-xs space-x-3 mt-2">
+           <div className="name text-white">John Doe</div>
+            <div className="date">{new Date().toLocaleDateString('en-US')}</div>
+           </div>
           </div>
         </Link>
       </div>
