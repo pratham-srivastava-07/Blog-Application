82b286993474039a6054a10a2fa7e26eb9907ffd
Krish <prathamrajsri2003@gmail.com>
2024-02-20 22:52:19 +0530

    shaped imgs

--- app/components/Catagory.jsx
+++ app/components/Catagory.jsx
@@ -4,16 +4,23 @@ import Image from 'next/image'
 // import styles from './catagories.module.css'
 
 const Catagory = () => {
+  const styles = {
+    borderRadius: "50%",
+    width: 50,
+    height: 50,
+    maxWidth: "unset",
+  }
   return (
     <div className='container lg:text-auto md:text-center md:ml-10 sm:ml-10 sm:text-center '>
       <h1 className="title mt-30 text-white font-bold lg:ml-28">Popular Catagories</h1>
       <div className="flex flex-wrap lg:ml-40 justify-between mt-5 border-white gap-4">
         <Link href="/blog?cat=style" className="category flex w-48 text-center text-white items-center capitalize w-1/7 h-20 justify-center rounded-lg bg-blue-500 bg-opacity-30">
-          <Image 
+          <Image
            src="/style.png" 
            alt="style"
-           width={32}
-           height={32}
+           width={50}
+           height={50}
+           style={styles}
            className='rounded-full ml-3 mr-3 '
            />
            Style
@@ -22,8 +29,9 @@ const Catagory = () => {
         <Image 
            src="/fashion.png" 
            alt="fashion"
-           width={32}
-           height={32}
+           width={50}
+           height={50}
+           style={styles}
            className='rounded-full ml-3 mr-3'
            />
            Fashion
@@ -32,8 +40,9 @@ const Catagory = () => {
           <Image 
           src="/food.png" 
           alt="food"
-          width={32}
-          height={32}
+          width={50}
+          height={50}
+          style={styles}
           className='rounded-full ml-3 mr-3'
           />
           Food
@@ -42,8 +51,9 @@ const Catagory = () => {
           <Image 
           src="/travel.png" 
           alt="travel"
-          width={32}
-          height={32}
+          width={50}
+          height={50}
+          style={styles}
           className='rounded-full ml-3 mr-3'
           />
           Travel
@@ -52,8 +62,9 @@ const Catagory = () => {
           <Image 
           src="/culture.png" 
           alt="culture"
-          width={32}
-          height={32}
+          width={50}
+          height={50}
+          style={styles}
           className='rounded-full ml-3 mr-3'
           />
           Culture
@@ -62,8 +73,9 @@ const Catagory = () => {
           <Image 
           src="/coding.png" 
           alt="coding"
-          width={32}
-          height={32}
+          width={50}
+          height={50}
+          style={styles}
           className='rounded-full ml-3 mr-3'
           />
           Coding
