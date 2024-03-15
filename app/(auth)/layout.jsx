99ad4c5b92020c1dd7266db30276b3a12485f459
Krish <prathamrajsri2003@gmail.com>
2024-03-15 10:42:14 +0530

    .

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,7 @@
 export default function({children}) {
     return (
         <>
-        <div className="flex items-center justify-center text-white">
+        <div className="flex items-center justify-center text-white font-extrabold">
             EVERY PERSON STARS SOMEWHERE...
         </div>
         <div className="children">
